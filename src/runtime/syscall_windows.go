b9c29e269132afd8181201df190f457f2e49a839
Kristi Bektashi <kristibektashi@outlook.com>
2025-08-23 00:15:34 +0300

    Revert "runtime: remove remaining windows/arm files and comments"
    This reverts commit 9b0a507735e8c0210f7f8a1cb875836133c1cee9.

--- src/runtime/syscall_windows.go
+++ src/runtime/syscall_windows.go
@@ -98,14 +98,19 @@ func (p *abiDesc) assignArg(t *_type) {
 		// passed as two words (little endian); and
 		// structs are pushed on the stack. In
 		// fastcall, arguments larger than the word
-		// size are passed by reference.
+		// size are passed by reference. On arm,
+		// 8-byte aligned arguments round up to the
+		// next even register and can be split across
+		// registers and the stack.
 		panic("compileCallback: argument size is larger than uintptr")
 	}
 	if k := t.Kind(); GOARCH != "386" && (k == abi.Float32 || k == abi.Float64) {
 		// In fastcall, floating-point arguments in
 		// the first four positions are passed in
 		// floating-point registers, which we don't
-		// currently spill.
+		// currently spill. arm passes floating-point
+		// arguments in VFP registers, which we also
+		// don't support.
 		// So basically we only support 386.
 		panic("compileCallback: float arguments not supported")
 	}
@@ -122,7 +127,7 @@ func (p *abiDesc) assignArg(t *_type) {
 	// argument word and all supported Windows
 	// architectures are little endian, so srcStackOffset
 	// is already pointing to the right place for smaller
-	// arguments.
+	// arguments. The same is true on arm.
 
 	oldParts := p.parts
 	if p.tryRegAssignArg(t, 0) {
@@ -158,8 +163,8 @@ func (p *abiDesc) assignArg(t *_type) {
 		p.dstStackSize += t.Size_
 	}
 
-	// cdecl, stdcall, and fastcall pad arguments to word size.
-	// TODO(rsc): On arm64 do we need to skip the caller's saved LR?
+	// cdecl, stdcall, fastcall, and arm pad arguments to word size.
+	// TODO(rsc): On arm and arm64 do we need to skip the caller's saved LR?
 	p.srcStackSize += goarch.PtrSize
 }
 
@@ -256,7 +261,7 @@ const callbackMaxFrame = 64 * goarch.PtrSize
 //
 // On 386, if cdecl is true, the returned C function will use the
 // cdecl calling convention; otherwise, it will use stdcall. On amd64,
-// it always uses fastcall.
+// it always uses fastcall. On arm, it always uses the ARM convention.
 //
 //go:linkname compileCallback syscall.compileCallback
 func compileCallback(fn eface, cdecl bool) (code uintptr) {
@@ -351,6 +356,10 @@ type callbackArgs struct {
 	// For fastcall, the trampoline spills register arguments to
 	// the reserved spill slots below the stack arguments,
 	// resulting in a layout equivalent to stdcall.
+	//
+	// For arm, the trampoline stores the register arguments just
+	// below the stack arguments, so again we can treat it as one
+	// big stack arguments frame.
 	args unsafe.Pointer
 	// Below are out-args from callbackWrap
 	result uintptr
